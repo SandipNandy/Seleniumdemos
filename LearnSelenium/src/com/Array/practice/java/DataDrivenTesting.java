ad94d68dce06a30751626a097dfef8ee9272c42b
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-04-25 08:20:55 +0530

    Update DataDrivenTesting.java

--- LearnSelenium/src/com/Array/practice/java/DataDrivenTesting.java
+++ LearnSelenium/src/com/Array/practice/java/DataDrivenTesting.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class DataDrivenTesting {
+	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		System.setProperty("webdriver.chrome.driver","D:\\Softwares Required For Testing Batch\\seleniumGH20software\\chromedriver_win32 (1)\\chromedriver.exe");
 	    WebDriver driver= new ChromeDriver(); 
