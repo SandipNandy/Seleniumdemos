bb0ad0fe5b7fc206ec21a12cfde455f748c80d05
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-04-25 08:18:58 +0530

    Update Parrellel.java

--- LearnSelenium/src/listnerdemo/Parrellel.java
+++ LearnSelenium/src/listnerdemo/Parrellel.java
@@ -1,10 +1,12 @@
+
 package listnerdemo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
-public class Parrellel { 
+public class Parrellel {
+	
 	WebDriver driver;
 	@Test
 	public void method1() {
