737ed833050c0ad7923408fbcc56b797d9f88ae4
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-14 11:29:22 +0530

    Update ArrayReverseint.java

--- LearnSelenium/src/com/Array/practice/java/ArrayReverseint.java
+++ LearnSelenium/src/com/Array/practice/java/ArrayReverseint.java
@@ -1,11 +1,8 @@
 package com.Array.practice.java;
 public class ArrayReverseint {
-	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		int sum=0,remainder,n;
 		n=23;
-		
 //		Scanner sc=new Scanner(System.in);
 //		n=sc.nextInt();
 		while(n>=0)
