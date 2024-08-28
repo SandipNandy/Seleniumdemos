ba618d69f06c14b87b3e8e2ea03560f5b05a5a11
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-08-28 17:14:32 +0530

    Update AlertDemo.java

--- LearnSelenium/src/commandsdemo/AlertDemo.java
+++ LearnSelenium/src/commandsdemo/AlertDemo.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AlertDemo {
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",
