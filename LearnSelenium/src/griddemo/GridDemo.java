5a7630b3081c602d7416ec81d0cb913826c20da1
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-09-13 19:03:03 +0530

    Update GridDemo.java

--- LearnSelenium/src/griddemo/GridDemo.java
+++ LearnSelenium/src/griddemo/GridDemo.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
+
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 public class GridDemo {
