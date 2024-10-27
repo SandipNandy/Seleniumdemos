8fb6f1f9fc081b6d423d57ec36ceddde2acc6d3e
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-10-27 13:36:27 +0530

    Update TestGroup1.java

--- LearnSelenium/src/grouppackage/TestGroup1.java
+++ LearnSelenium/src/grouppackage/TestGroup1.java
@@ -3,6 +3,7 @@ package grouppackage;
 import org.testng.annotations.Test;
 
 public class TestGroup1 {
+	
 	 @Test(groups = { "smoke"})
 	  public void test1() {
 		 System.out.println("smoke test 1");
